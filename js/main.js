34f48da5d73f31f0501f42ccf36a975ae1f9d2c3
mango906 <minkb01@dgsw.hs.kr>
2019-06-04 12:22:20 +0900

    [ADD] createRoom & joinRoom

--- js/main.js
+++ js/main.js
@@ -1,24 +1,42 @@
 const axios = require('axios');
+const socket = io('http://localhost:8080');
 let id;
 
 window.onload = () => {
-  chrome.storage.sync.get('id', data => {
-    id = data.id;
-    getFriends();
+  socket.emit('getRooms');
+
+  socket.on('rooms', rooms => {
+    let friendNav = document.getElementById('friend-nav');
+    while (friendNav.firstChild) {
+      friendNav.removeChild(friendNav.firstChild);
+    }
+    rooms.forEach(f => {
+      let li = document.createElement('li');
+      li.classList.add('friend-item');
+      let childLi = document.createElement('li');
+      let img = document.createElement('img');
+      img.src = './../icons/phone-call.png';
+      img.onclick = call();
+      childLi.innerHTML = f.room_name;
+      li.appendChild(childLi);
+      li.appendChild(img);
+      friendNav.appendChild(li);
+    });
+    console.log(rooms);
   });
 
+  socket.on('createRoom', url => {
+    location.href = url;
+  });
+
+  // chrome.storage.sync.get('id', data => {
+  //   id = data.id;
+  //   getFriends();
+  // });
+
   document.getElementById('addBtn').addEventListener('click', () => {
-    let friendId = prompt('아이디를 입력해주세요.');
-    let req = {
-      userId: id,
-      friendId: friendId
-    };
-    axios.post('http://localhost:8080/friends', req).then(res => {
-      if (res.status === 200) {
-        alert('친구 추가 완료!');
-        getFriends();
-      }
-    });
+    let roomName = prompt('방 제목을 입력해주세요');
+    socket.emit('createRoom', roomName);
   });
 };
 
@@ -37,6 +55,7 @@ getFriends = () => {
         let childLi = document.createElement('li');
         let img = document.createElement('img');
         img.src = './../icons/phone-call.png';
+        img.onclick = call();
         childLi.innerHTML = f.user_name;
         li.appendChild(childLi);
         li.appendChild(img);
@@ -47,4 +66,8 @@ getFriends = () => {
     .catch(err => {
       console.error(err);
     });
+};
+
+call = () => {
+  // location.href = './../call.html';
 };
